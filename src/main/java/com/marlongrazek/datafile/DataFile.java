7e2c0da875fb3880516b78a789f0eb8e130f8ee0
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-02 10:24:05 +0100

    new methods added

--- src/main/java/com/marlongrazek/datafile/DataFile.java
+++ src/main/java/com/marlongrazek/datafile/DataFile.java
@@ -52,6 +52,14 @@ public class DataFile {
         save();
     }
 
+    public void delete(String path) {
+        config.set(path, null);
+    }
+
+    public boolean isEmpty() {
+        return config.getConfigurationSection("").getKeys(false).isEmpty();
+    }
+
     public Boolean contains(String string) {
         return config.contains(string);
     }
@@ -60,24 +68,48 @@ public class DataFile {
         return config.get(path);
     }
 
+    public boolean getBoolean(String path) {
+        return config.getBoolean(path);
+    }
+
+    public boolean getBoolean(String path, boolean def) {
+        return config.getBoolean(path, def);
+    }
+
     public String getString(String path) {
         return config.getString(path);
+    }
+
+    public String getString(String path, String def) {
+        return config.getString(path, def);
+    }
+
+    public List<String> getStringList(String path) {
+        return config.getStringList(path);
     }
 
     public int getInt(String path) {
         return config.getInt(path);
     }
 
+    public int getInt(String path, int def) {
+        return config.getInt(path, def);
+    }
+
     public double getDouble(String path) {
         return config.getDouble(path);
     }
 
-    public boolean getBoolean(String path) {
-        return config.getBoolean(path);
+    public double getDouble(String path, double def) {
+        return config.getDouble(path, def);
     }
 
     public long getLong(String path) {
         return config.getLong(path);
+    }
+
+    public long getLong(String path, long def) {
+        return config.getLong(path, def);
     }
 
     public List<Long> getLongList(String path) {
